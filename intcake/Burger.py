992c36b5ce6a43ecdbd0258eb9621e53f07884ac
Chandra Prasetya Suardy <chazeps@gmail.com>
2015-11-25 01:26:42 +0700

    Updated intcake

--- intcake/Burger.py
+++ intcake/Burger.py
@@ -1,18 +1,19 @@
 def mergeSort(x):
-	if len(x) == 1:
-		return x
-	else:
-		return merge(mergeSort(x[0:(len(x)//2)]), mergeSort(x[(n//2 + 1):len(x)]))
+    if len(x) == 1:
+        return x
+    else:
+        return merge(mergeSort(x[0:(len(x) // 2)]), mergeSort(x[(n // 2 + 1):len(x)]))
 
-def merge(l,r):
-	res = []
-	while isSorted:
-		i,j=0,0
-		if i<len(l):
-			pass
-		if l[i] <= j:
-			res.append(l[i])
-			i+=1
-		else:
-			res.append(r[j])
-			j+=1
+
+def merge(l, r):
+    res = []
+    while isSorted:
+        i, j = 0, 0
+        if i < len(l):
+            pass
+        if l[i] <= j:
+            res.append(l[i])
+            i += 1
+        else:
+            res.append(r[j])
+            j += 1
--- intcake/HighestProductOf3.py
+++ intcake/HighestProductOf3.py
@@ -1,26 +1,26 @@
 def highest_product(l):
-	a,b,c = 0,0,0
-	negativeA,negativeB=0,0
-	for x in l:
-		if x >= 0:
-			if x > a:
-				c=b
-				b=a
-				a=x
-			elif x > b:
-				c=b
-				b=x
-			elif x > c:
-				c=x
-		else:
-			if x < negativeA:
-				negativeB=negativeA
-				negativeA=x
-			elif x < negativeB:
-				negativeB=x
-			
-	return a*max(b*c,negativeA*negativeB)
+    a, b, c = 0, 0, 0
+    negativeA, negativeB = 0, 0
+    for x in l:
+        if x >= 0:
+            if x > a:
+                c = b
+                b = a
+                a = x
+            elif x > b:
+                c = b
+                b = x
+            elif x > c:
+                c = x
+        else:
+            if x < negativeA:
+                negativeB = negativeA
+                negativeA = x
+            elif x < negativeB:
+                negativeB = x
 
-l = [-10,-10,1,3,2]
+    return a * max(b * c, negativeA * negativeB)
+
+l = [-10, -10, 1, 3, 2]
 print(l)
 print(highest_product(l))
