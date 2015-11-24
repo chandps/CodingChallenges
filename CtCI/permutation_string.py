2f3c1425bb4b96593284bb12f8f8248d20ad8e7e
Chandra Prasetya Suardy <chazeps@gmail.com>
2015-11-25 00:59:04 +0700

    Updated: permutation_string

--- CtCI/permutation_string.py
+++ CtCI/permutation_string.py
@@ -1,23 +1,27 @@
 def perm(x):
-	if len(x) == 2:
-		return [x, x[1]+x[0]]
-	newString = x[-1]
-	return join(perm(x[:-1]), newString)
+    if len(x) == 2:
+        return [x, x[1] + x[0]]
+    newString = x[-1]
+    return join(perm(x[:-1]), newString)
 
 
 def join(l, a):
-	result = []
-	for eachElement in l:
-		for eachLocation in range(len(eachElement)):
-			if eachLocation == 0:
-				result.append(a+eachElement)
-			elif eachLocation == (len(eachElement)-1):
-				result.append(eachElement[:eachLocation]+a+eachElement[eachLocation:])
-				result.append(eachElement+a)
-			else:
-				result.append(eachElement[:eachLocation]+a+eachElement[eachLocation:])
-	return result
+    result = []
+    for eachElement in l:
+        for eachLocation in range(len(eachElement)):
+            if eachLocation == 0:
+                result.append(a + eachElement)
+            elif eachLocation == (len(eachElement) - 1):
+                result.append(
+                    eachElement[:eachLocation] +
+                    a + eachElement[eachLocation:])
+                result.append(eachElement + a)
+            else:
+                result.append(
+                    eachElement[:eachLocation] +
+                    a + eachElement[eachLocation:])
+    return result
 
 res = perm(input("Insert string: "))
 print(res)
-print('len: ',len(res))
+print('len: ', len(res))
