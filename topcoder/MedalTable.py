cd3968d8d570b8b46f36f15909f0a22c9780bc24
Chandra Prasetya Suardy <chazeps@gmail.com>
2015-12-19 12:38:21 +0700

    Added: MedalTable

--- topcoder/MedalTable.py
+++ topcoder/MedalTable.py
@@ -1,57 +1,37 @@
+class Country():
+
+    def __init__(self, name, gold, silver, bronze):
+        self.name = name
+        self.gold, self.silver, self.bronze = gold, silver, bronze
+
+    def __str__(self):
+        return "%s %d %d %d" % (self.name, self.gold, self.silver, self.bronze)
+
+    def __lt__(self, other):
+        if self.gold == other.gold:
+            if self.silver == other.silver:
+                if self.bronze == other.bronze:
+                    return self.name > other.name
+                return self.bronze < other.bronze
+            return self.silver < other.silver
+        return self.gold < other.gold
+
+
 class MedalTable:
 
-    class country():
+    def generate(self, results):
+        trophy = {}
 
-        def __init__(self, name, gold, silver, bronze):
-            self.name = name
-            self.gold, self.silver, self.bronze = gold, silver, bronze
+        for cList in results:
+            for i, cname in enumerate(cList.split()):
+                if cname not in trophy:
+                    trophy[cname] = Country(cname, 0, 0, 0)
+                if i == 0:
+                    trophy[cname].gold = trophy[cname].gold + 1
+                if i == 1:
+                    trophy[cname].silver = trophy[cname].silver + 1
+                if i == 2:
+                    trophy[cname].bronze = trophy[cname].bronze + 1
+        trophyList = tuple(trophy.values())
 
-        def __str__(self):
-            return "%s %d %d %d" % (self.name, self.gold, self.silver, self.bronze)
-
-        def __lt__(self, other):
-            if self.gold == other.gold:
-                if self.silver == other.silver:
-                    if self.bronze == other.bronze:
-                        return self.name < other.name
-                    return self.bronze < other.bronze
-                return self.silver < other.silver
-            return self.gold < other.gold
-
-    data = []
-    data.append(country("KOR", 5, 0, 0))
-    data.append(country("JPY", 3, 1, 2))
-    data.append(country("CHN", 3, 1, 3))
-    data.append(country("HKG", 4, 0, 1))
-
-    data.sort()
-    for x in data:
-        print(x)
-
-"""    def generate(self, results):
-        gold = [[]] * 1000
-        silver = [[]] * 1000
-        bronze = [[]] * 1000
-        trophy = [[] * 1000, [] * 1000, [] * 1000]
-        database = {}
-        for countryList in results:
-            countryList = countryList.split()
-
-            for i, country in enumerate(countryList):
-                if country not in database:
-                    database[country] = [0, 0, 0]
-
-                lastPoint = database[country][i]
-                database[country][i] = lastPoint + 1
-
-                if country not in trophy[i][lastPoint]:
-                    trophy[i][lastPoint + 1].append(country)
-                else:
-                    del trophy[i][lastPoint]
-                    trophy[i][lastPoint + 1].append(country)
-
-            result = ()
-            for x in range(1000, 0, -1):
-                if len(trophy[0][x]) > 0:
-
-                trophy[0][x]"""
+        return sorted(trophyList, reverse=True)
