b0d010e8e6f8f1fa98ad743546c34404b7950e73
Chandra Prasetya Suardy <chazeps@gmail.com>
2015-12-20 14:00:20 +0700

    Added: Smart word toy

---
+++ topcoder/SmartWordToy.py
@@ -0,0 +1,9 @@
+ def minPresses(start, finish, forbid):
+
+    return -1
+
+
+start = "aaaa"
+finish = "zzzz"
+forbid = ("a a a z", "a a z a", "a z a a", "z a a a", "a z z z", "z a z z", "z z a z", "z z z a")
+Returns: 8
