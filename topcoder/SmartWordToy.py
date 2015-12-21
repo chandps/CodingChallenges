0ebae7290725db6e51bb864b84bf37e7400aadfc
Chandra Prasetya Suardy <chazeps@gmail.com>
2015-12-21 21:16:13 +0700

    Added: Smart word toy

--- topcoder/SmartWordToy.py
+++ topcoder/SmartWordToy.py
@@ -1,9 +1,28 @@
- def minPresses(start, finish, forbid):
+def minPresses(start, finish, forbid):
+    s, f = [], []
+    fbd = {}
+    i = 0
+    while i < 4:
+        s.append(ord(start[i]))
+        f.append(ord(finish[i]))
+
+    for element in forbid:
+        element = forbid.split()
+        for compound in element:
+            for character in compound:
+                fbd[character] = True
+
+    finish = False
+    while not finish:
+
+        c[i]
 
     return -1
 
 
 start = "aaaa"
 finish = "zzzz"
-forbid = ("a a a z", "a a z a", "a z a a", "z a a a", "a z z z", "z a z z", "z z a z", "z z z a")
-Returns: 8
+forbid = ("a a a z", "a a z a", "a z a a", "z a a a",
+          "a z z z", "z a z z", "z z a z", "z z z a")
+# Returns: 8
+print(minPresses(start, finish, forbid))
