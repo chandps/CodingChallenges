b3e06de2d40f72a8662614516bff149f29aec3d7
chandps <chazeps@gmail.com>
2015-12-29 14:47:16 +0700

    Updated: Binary Gap

--- cdlt/BinaryGap.py
+++ cdlt/BinaryGap.py
@@ -1,2 +1,25 @@
 def solution(N):
-    pass
+    n = bin(N)[2::]
+    maxC = 0
+    count = 0
+    enter = False
+    for x in n:
+        if x == "1":
+            if enter is False:
+                enter = True
+            else:
+                if count > maxC:
+                    maxC = count
+                count = 0
+        elif x == "0":
+            if enter is True:
+                count += 1
+    return maxC
+
+
+def print_solution(N):
+    print(bin(N)[2::])
+    print(solution(N))
+
+
+print_solution(561892)
