9a47a351c32e60a344923680f4e6283434329974
Chandra Suardy <chazeps@gmail.com>
2017-07-05 07:59:31 +0700

    Added: basic data types

---
+++ Hackerrank/intro/basicDataTypes.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+void basicDataTypes() {
+	
+	int i;
+	long l;
+	char c;
+	float f;
+	double d;
+
+	scanf("%d %ld %c %f %lf", &i, &l, &c, &f, &d);
+	printf("%d\n", i);
+	printf("%ld\n", l);
+	printf("%c\n", c);
+	printf("%f\n", f);
+	printf("%lf", d);
+
+}
