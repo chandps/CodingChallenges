d94ca55f308de266776ca30a6a5152f5bfa86456
Chandra Suardy <chazeps@gmail.com>
2018-01-02 19:04:19 +0700

    Fixed negative number bugs

--- leetcode/TwoSum.cs
+++ leetcode/TwoSum.cs
@@ -3,13 +3,23 @@ public class Solution {
         
         int array_size = (nums.Max() > target) ?  nums.Max() + 1 : target + 1;
         int[] nums_count = new int[array_size];
+		int[] nums_count_negative = new int[array_size];
         int[] nums_count_index = new int[array_size];
+		int[] nums_count_index_negative = new int[array_size];
         int[] result = new int[2];
         
-        
+        //count occurences for positive and negative numbers
         for(int i = 0; i < nums.Length; i++){
-            nums_count[nums[i]]++;
-            nums_count_index[nums[i]] = i;
+			
+			if(nums[i] >= 0){
+				nums_count[nums[i]]++;
+				nums_count_index[nums[i]] = i;
+			}
+			else {
+				nums_count_negative[nums[i]]++;
+				nums_count_index_negative[nums[i]] = i;
+			}
+			
         }
         
         bool isFound = false;
