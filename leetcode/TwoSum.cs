47d6360d8addd1efd5cb452356b89c6a23b642f4
Chandra Suardy <chazeps@gmail.com>
2018-01-04 20:00:12 +0700

    BugFix

--- leetcode/TwoSum.cs
+++ leetcode/TwoSum.cs
@@ -1,13 +1,18 @@
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
         
-        int array_size = (nums.Max() > target) ?  nums.Max() + 1 : target + 1;
-        int[] nums_count = new int[array_size];
-		int[] nums_count_negative = new int[array_size];
-        int[] nums_count_index = new int[array_size];
-		int[] nums_count_index_negative = new int[array_size];
+		int max_positive_number = nums.Max();
+		int max_negative_number = nums.Min();
+		
+        int array_size_positive = (max_positive_number > target) ?  max_positive_number + 1 : target + 1;
+		int array_size_negative = (max_negative_number < target) ? (max_negative_number * -1) + 1 : (target * -1) + 1;
+        int[] nums_count = new int[array_size_positive];
+		int[] nums_count_negative = new int[array_size_negative];
+        int[] nums_count_index = new int[array_size_positive];
+		int[] nums_count_index_negative = new int[array_size_negative];
         int[] result = new int[2];
         
+		
         //count occurences for positive and negative numbers
         for(int i = 0; i < nums.Length; i++){
 			
@@ -21,6 +26,52 @@ public class Solution {
 			}
 			
         }
+		
+		// iterate to target, check the complement, if nothing exist, iterate again to max positive number
+		bool outOfBounds = false;
+		int i = 0;
+		
+		while(!outOfBounds) {
+			//check out of bonds
+			if (i <= target) {
+				if (i > max_positive_number) {
+					outOfBounds = true;
+					continue;
+				}				
+			}
+			else if(i > target) {
+				if (i-target < max_negative_number) {
+					outOfBounds = true;
+					continue;
+				}
+			}
+		
+			if (i < target) {
+				if (nums_count[i] + nums_count[target - i] >= 2) {
+					result[0] = nums_count_index[i];
+					result[1] = nums_count_index[target - i];
+					
+					outOfBounds = true;
+				}
+			}
+			else { //i > target
+				
+			}
+			
+			
+			outOfBounds = true;
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		// check which index is larger (- or +)
 		
 		// case 1: target is + number
